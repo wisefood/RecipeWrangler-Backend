f202dee9b0065158a2feb72ebe49f94dbd5c9d1a
petroud <petroudimitris@outlook.com>
2026-01-15 17:21:44 +0000

    Parameters for Neo4j driver are set by env not directly in the code

--- src/recipe_wrangler/utils/neo4j_utils.py
+++ src/recipe_wrangler/utils/neo4j_utils.py
@@ -1,13 +1,11 @@
 from neo4j import GraphDatabase
 from typing import Optional
+import os
 
 # Set up the connection
-uri = "bolt://localhost:7687"  # Change if using a remote server or different port
-
-# Create the driver instance
-
-username = "neo4j"
-password = "password123"
+uri = os.getenv("NEO4J_URI", "bolt://localhost:7687")
+username = os.getenv("NEO4J_USERNAME", "neo4j")
+password = os.getenv("NEO4J_PASSWORD", "password123")
 
 driver = GraphDatabase.driver(uri, auth=(username, password))
 
